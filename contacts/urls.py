bb692f7c36ccc4d3de0772e35ecfa1ae8f291800
Salami Idris Akolade <salamiidris2007@gmail.com>
2026-05-03 15:59:29 +0100

    Added HTML template to dynamically display contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # When someone visits /api/contacts/, trigger the 'all_contacts' view
+    # Existing JSON API route
     path('api/contacts/', views.all_contacts, name='all_contacts'),
+
+    # Brand New HTML web page route
+    path('pages/contacts/', views.html_contact_list, name='html_contacts'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
+from django.shortcuts import render  # <--- Added this new tool here!
 from .models import Contact
 
 
+# --- EXISTING JSON API (Safe and untouched) ---
 def all_contacts(request):
     # Query the database for all contacts
     contacts_query = Contact.objects.all().values('first_name', 'last_name', 'phone_number')
@@ -9,3 +11,12 @@ def all_contacts(request):
     # Convert the query to a standard Python list and send it back as JSON
     contacts_list = list(contacts_query)
     return JsonResponse({'contacts': contacts_list})
+
+
+# --- BRAND NEW HTML VIEW (Added to the bottom) ---
+def html_contact_list(request):
+    # 1. Fetch all contacts from the database
+    all_my_contacts = Contact.objects.all()
+
+    # 2. Render the HTML file and pass the database records into it
+    return render(request, 'contact_list.html', {'contacts': all_my_contacts})
